e680afd4d699d1d6fbe1f9d4a5fd01cc2a243d5e
millermann <118037014+millermann@users.noreply.github.com>
2024-09-26 19:52:11 -0300

    test

---
+++ src/pmi2024/PMI2024.java
@@ -0,0 +1,12 @@
+package pmi2024;
+
+public class PMI2024 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("sape");
+    }
+    
+}
