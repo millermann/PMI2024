d8cd29c4752d6110ee1be5c21df0d563403725a5
millermann <118037014+millermann@users.noreply.github.com>
2024-10-01 16:48:33 -0300

    Delete PMI2024-main directory

--- PMI2024-main/PMI2024/src/estructuraBase/main.java
+++
@@ -1,9 +0,0 @@
-package estructuraBase;
-
-public class main {
-    public static void main(String[] arg){
-        Software lolo;
-        
-       
-    }
-}
--- PMI2024-main/src/pmi2024/PMI2024.java
+++
@@ -1,9 +0,0 @@
-package pmi2024;
-
-public class PMI2024 {
-
-    public static void main(String[] args) {
-        System.out.print("sape");
-    }
-    
-}
