b8249f3e4f563949399df5c2e98ad558a956c215
millermann <118037014+millermann@users.noreply.github.com>
2024-10-09 19:52:23 -0300

    ahora si cambió el main del GUI

--- src/pmi2024/GUI.java
+++ src/pmi2024/GUI.java
@@ -1,5 +1,6 @@
 package pmi2024;
 
+import fecha.Fecha;
 import ingenieros.Ingeniero;
 import java.awt.Toolkit;
 import java.util.ArrayList;
@@ -1418,17 +1419,28 @@ public class GUI extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
+        
+        ArrayList<Ingeniero> arrayIngenieros = new ArrayList<>();
+        ArrayList<ProyectoTecnologico> arrayProyectos = new ArrayList<>();
+        ArrayList<RecursoTecnologico> arrayRecursos = new ArrayList<>();
+        
+        arrayProyectos.add(new DesarrolloDeSoftware("Facebook", "Red Social", "La vamos a re pegar negritooo", (float) 1000, new Fecha(12,12,2000), new Fecha()));
+        arrayProyectos.add(new DesarrolloDeSoftware("Instagram", "Red Social", "la de las fotos y las trolas", (float) 1000000, new Fecha(12,12,2009), new Fecha()));
+        arrayProyectos.add(new DesarrolloDeHardware("Nokia 1100", "Telefono Celular", "Duro durito", (float) 1800200, new Fecha(12,12,2000), new Fecha(2,12,2005)));
+        arrayIngenieros.add(new Ingeniero("Paco", "Zabala", "loquito@gmail.com", "flautista", new Fecha()));
+        arrayIngenieros.add(new Ingeniero("Carlos", "Garcia", "snm@gmail.com", "clavadista", new Fecha()));
+        arrayIngenieros.add(new Ingeniero("lolo", "Garcia", "snm@gmail.com", "clavadista", new Fecha()));
+        arrayIngenieros.add(new Ingeniero("pepe", "Garcia", "snm@gmail.com", "clavadista", new Fecha()));
+        arrayRecursos.add(new RecursoTecnologico("Servidor", "una coradir del 2011"));
+        arrayRecursos.add(new RecursoTecnologico("Destornillador", "ha sido robado..."));
 
-        /* Create and display the form 
+        //Create and display the form 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new GUI().setVisible(true);
+                new GUI(arrayIngenieros, arrayProyectos, arrayRecursos).setVisible(true);
             }
         });
-         */
+        
     }
 
 
